b0fa25bd441f7f44ab6920d62f833c4a4ac7f9c4
Eli <ebcitron@gmail.com>
2021-09-22 17:54:41 -0700

    testing  out individual functions to specify data to return, not optimal

--- script.py
+++ script.py
@@ -18,5 +18,28 @@ events = json.load(open("./events.json"))
 
 # Figuring out whats most efficient in terms of adding the hours in between the 9-5 workdays in as their own scheduled times to be worked around.
 
+x = "John"
+#Splitting up events[] by user_id
+# Checking lookingFor[] inputs for validity and their user_id
 
+#Returns the id associated with the name its fed
+def getId(name):
+    foundId = next((person['id'] for person in users if person['name'] == name), False)
+    return foundId
+
+#Returns events associated with that user_id
+def getEvents(foundId):
+    return [event for event in events if event['user_id'] == foundId]
+
+
+#Testing
+foundId = getId(x)
+
+eventsById = getEvents(foundId)
+
+
+
+print(foundId)
+print(eventsById)
+#def eventsById
 
